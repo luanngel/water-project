ebccdcebb067d182604c58d9bbbe075079614a89
Esteban <exteban16@gmail.com>
2025-12-19 00:27:38 -0600

    Delete comments

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   plugins: [react(),tailwindcss()],
 
   server: {
-    host: '0.0.0.0', // Esto permite que el servidor escuche en todas las IP disponibles
-    port: 5173, // Puerto por defecto de Vite (puedes cambiarlo si lo deseas)
+    host: '0.0.0.0',
+    port: 5173,
   },
 
 })

8bb4942caf3d45327a035c20bc0e80c0819e51e7
Esteban <exteban16@gmail.com>
2026-01-07 00:49:37 -0600

    vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+    })
+  ],
   server: {
-    allowedHosts: [
-      "localhost",
-      "127.0.0.1",
-      "reyna-compressive-shaunna.ngrok-free.dev",
-      "https://sistema.gestionrecursoshidricos.com/"
-    ],
+    host: true,
     port: 5173,
+    allowedHosts: [
+      'sistema.gestionrecursoshidricos.com'
+    ],
+    hmr: {
+      overlay: true,
+    },
+    // proxy:{
+    //   '/api':{
+    //     target: 'http://localhost:4000/',
+    //     changeOrigin : true,
+    //     secure: false,
+    //   }
+    // }
   },
-});
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom'],
+  },
+})

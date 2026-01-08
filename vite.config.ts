215b197572090fc86882ea5a781848217cd86273
Exteban08 <exteban16@gamil.com>
2026-01-08 02:06:32 +0000

    Vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,33 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+    }),
+    tailwindcss(),
+  ],
   server: {
-    allowedHosts: [
-      "localhost",
-      "127.0.0.1",
-      "reyna-compressive-shaunna.ngrok-free.dev",
-    ],
+    host: true,
     port: 5173,
+    allowedHosts: [
+      'localhost',
+      'sistema.gestionrecursoshidricos.com',
+    ],
+    hmr: {
+      overlay: true,
+    },
+    // proxy:{
+    //   '/api':{
+    //     target: 'http://localhost:4000/',
+    //     changeOrigin : true,
+    //     secure: false,
+    //   }
+    // }
   },
-});
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom'],
+  },
+})
